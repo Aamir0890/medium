402a85532168822af66bd68f2e6bb3b85d84859c
Aamirheart <aamir@heartitout.in>
2024-12-18 12:42:35 +0530

    security

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,15 @@
 import { Hono } from 'hono'
+import {User} from '@prisma/client'
 
-const app = new Hono()
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL:string;
+  }
+}>
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
@@ -8,6 +17,12 @@ app.get('/', (c) => {
 
 
 app.post('/api/v1/user/signup',(c)=>{
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate())
+  
+
   return c.text("hello")
 })
 
